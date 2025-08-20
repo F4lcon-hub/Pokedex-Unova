c02c2b46087fa8c202320d4ee601ff0e1c58d989
F4lcon-hub <delf040603@gmail.com>
2025-08-20 13:22:13 -0300

    Pokedex Unova

--- app.py
+++ app.py
@@ -1,11 +1,14 @@
-
+import os
 from flask import Flask
 from rotas import rotas
-
 
 app = Flask(__name__)
 app.register_blueprint(rotas)
 
 if __name__ == '__main__':
-    # Executa o servidor Flask em modo debug
+    # Para desenvolvimento local
     app.run(debug=True)
+else:
+    # Para produção (Render)
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port, debug=False)
